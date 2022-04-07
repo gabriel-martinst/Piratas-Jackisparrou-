6cb6a52878752850e2e9baa82acc3fc208468d25
andressalessa <lessadasilveiraandressa@gmail.com>
2022-04-07 20:36:18 -0300

    Add files via upload

--- js/Cannon.js
+++ js/Cannon.js
@@ -5,14 +5,28 @@ class Cannon {
         this.y = y;
         this.width = width;
         this.height = height;
-        this.angle = angle;    
+        this.angle = angle;
+        this.cannonBaseImg = loadImage("./assets/cannonBase.png");
+        this.cannonTopImg = loadImage("./assets/canon.png"); 
     }
+
     display()
     {
-        rect(this.x, this.y, this.width, this.height, this.angle);
+        if (keyIsDown(RIGHT_ARROW) && this.angle < 70) {
+            this.angle += 1;
+        }
+
+        if (keyIsDown(LEFT_ARROW) && this.angle > -30) {
+            this.angle -= 1;
+        }
+
         push();
+        translate(this.x, this.y);
+        rotate(this.angle);
         imageMode(CENTER);
+        image(this.cannonTopImg, 0, 0, this.width, this.height);
         pop();
+        image(this.cannonBaseImg, 70, 20, 200, 200);
     }
     
 }
